715117239e5e23d34f980da7361185389cca78d0
593842659@qq.com <593842659@qq.com>
2020-07-23 17:33:46 +0800

    构建项目 配置全局样式 el-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>这个是登入页面</div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      
+    }
+  },
+};
+</script>
+
+<style>
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
+// vue.config.js
 const path = require('path');
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin; // 打包分析
-const IS_PROD = ['production'].includes(process.env.NODE_ENV);
+
+const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV);
 const resolve = (dir) => path.join(__dirname, dir);
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/site/vue-demo/' : '/', // 公共路径
+  publicPath: process.env.NODE_ENV === 'production' ? '/site/vue-demo/' : '/',  // 公共路径
   indexPath: 'index.html', // 相对于打包路径index.html的路径
   outputDir: process.env.outputDir || 'dist', // 'dist', 生产环境构建文件的目录
   assetsDir: 'static', // 相对于outputDir的静态资源(js、css、img、fonts)目录
@@ -26,20 +28,8 @@ module.exports = {
       .set('@components', resolve('src/components'))
       .set('@views', resolve('src/views'))
       .set('@store', resolve('src/store'));
-    // 压缩图片
-    // 需要 npm i -D image-webpack-loader
-    // config.module
-    //   .rule("images")
-    //   .use("image-webpack-loader")
-    //   .loader("image-webpack-loader")
-    //   .options({
-    //     mozjpeg: { progressive: true, quality: 65 },
-    //     optipng: { enabled: false },
-    //     pngquant: { quality: [0.65, 0.9], speed: 4 },
-    //     gifsicle: { interlaced: false },
-    //     webp: { quality: 75 }
-    //   });
-    // 打包分析, 打包之后自动生成一个名叫report.html文件(可忽视)
+    // 打包分析
+    // 打包之后自动生成一个名叫report.html文件(可忽视)
     if (IS_PROD) {
       config.plugin("webpack-report").use(BundleAnalyzerPlugin, [
         {
@@ -48,33 +38,15 @@ module.exports = {
       ]);
     }
   },
-  configureWebpack: config => {
-    // 开启 gzip 压缩
-    // 需要 npm i -D compression-webpack-plugin
-    const plugins = [];
-    if (IS_PROD) {
-      plugins.push(
-        new CompressionWebpackPlugin({
-          filename: "[path].gz[query]",
-          algorithm: "gzip",
-          test: productionGzipExtensions,
-          threshold: 10240,
-          minRatio: 0.8
-        })
-      );
-    }
-    config.plugins = [...config.plugins, ...plugins];
-  },
+  configureWebpack: config => { },
   css: {
     extract: IS_PROD,
     requireModuleExtension: false,// 去掉文件名中的 .module
     loaderOptions: {
       // 给 less-loader 传递 Less.js 相关选项
-      less: {
+      sass: {
         // `globalVars` 定义全局对象，可加入全局变量
-        globalVars: {
-          primary: '#333'
-        }
+        prependData: `@import "./src/styles/main.scss";`
       }
     }
   },
@@ -88,7 +60,7 @@ module.exports = {
     https: false, // https:{type:Boolean}
     open: false, //配置自动启动浏览器
     hotOnly: true, // 热更新
-    // proxy: 'http://localhost:8080' // 配置跨域处理,只有一个代理
+    // proxy: 'http://localhost:8080'   // 配置跨域处理,只有一个代理
     proxy: { //配置多个跨域
       "/api": {
         target: "http://172.11.11.11:7071",
